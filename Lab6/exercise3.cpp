b7e8d94f6e0b47e208036dc5cc5c669929dd543e
aya-soghayyer <128791822+aya-soghayyer@users.noreply.github.com>
2026-05-10 05:04:37 +0200

    update on exercise 3 Lab6

--- Lab6/exercise3.cpp
+++ Lab6/exercise3.cpp
@@ -3,59 +3,64 @@
 using namespace std;
 
 int main() {
-	ifstream fin;
-	char x;
-	int choice;
-	cout << "1- Capitals & smalls\n"
-		 <<	"2- Vowels & consonants\n"
-		 <<	"3- Exit\n"
-		 <<	"Enter your choice: ";	
+	ifstream fin; 
+char x; 
+int choice; 
+do {
+	system("cls"); 
+	cout << "1- capitals & smals\n"; 
+	cout << "2- Vowels & consonants\n"; 
+	cout << "3- Exit\n";
+	cout << "Enter your choice: ";
 	cin >> choice;
-	fin.open("data.txt");
-	if(!fin) {
-		cout << "file not found!\n";
-		system("pause");
-		return 0;
+	fin.open("letter.txt"); 
+	if (!fin) {
+		cout << "File not found\n";
+		return 0; 
 	}
-	switch(choice) {
+	switch (choice) {
 	case 1:
-		{
-		int cap=0,small=0;
-		fin >> x;
-		while(fin) {
-			if(x >= 'A' && x <= 'Z')
-				cap++;
-			else if(x >= 'a' && x <='z')
-				small++;
-			fin >> x;
+	{
+		int cap = 0, sm = 0; 
+		fin >> x; 
+		while (fin) {
+			if (x >= 'A' && x < 'Z')
+				cap++; 
+			else if (x >= 'a' && x < 'z')
+				sm++;
+			fin >> x; 
 		}
 		fin.close();
-		cout << "number of capital letters: " << cap << endl;
-		cout << "number of small letters: " << small << endl;
+		cout << "The Number of capital letters: " << cap << endl;
+		cout << "The number of small letters: " << sm << endl;
+		system("puase"); 
 		break;
-		}
+	}
 	case 2:
-		{
-		int vol=0,con=0;
+	{
+		int vow = 0, con = 0;
 		fin >> x;
-		while(fin) {
-			if(x == 'a' || x == 'A' || x == 'e' || x == 'E'
-				|| x == 'i' || x == 'I' || x == 'o' || x == 'O'
-				|| x == 'u' || x == 'U')
-				vol++;
-			else if(x >= 'a' && x <= 'z' || x >= 'A' && x <= 'Z')
+		while (fin) {
+			if (x == 'a' || x == 'A'|| x == 'e' || x == 'E'|| x == 'o' || x == 'O' ||
+				x == 'i' || x == 'I'|| x == 'u' || x == 'U')
+				vow++;
+			else if (x >= 'a' && x < 'z' || x >= 'A' && x < 'Z')
 				con++;
 			fin >> x;
 		}
 		fin.close();
-		cout << "number of vowel letters: " << vol << endl;
-		cout << "number of consonant letters: " << con << endl;
-		break;
-		}
-	case 3: break;
-	default: cout << "invalid input\n";
+		cout << "The Number of vowel letters: " << vow << endl;
+		cout << "The number of consonant letters: " << con << endl;
+		break; 
+	}
+	default: {
+		cout << "Invalid input\n"; 
+		break; 
+	}
 
 	}
+	
+} while (choice != 3);
 	
 	system("pause");
 	return 0;
